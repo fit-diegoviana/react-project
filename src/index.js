43f9c66bbabcdf66d2e224f67e5916a29f819b1c
Diego <dtviana90@gmail.com>
2022-10-13 22:06:09 -0300

    feat: Cria segundo componente com propriedades

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import ReactDOM from "react-dom"
 import React from 'react'
 import './index.css'
 import Primeiro from "./Components/basicos/PrimeiroComponente"
+import ComParametro from "./Components/basicos/ComParametro"
 
 const tag = <strong> Oláa React!!</strong>
 ReactDOM.render
@@ -9,6 +10,14 @@ ReactDOM.render
     <div>
         {tag}
         <Primeiro></Primeiro>
+        <ComParametro 
+         titulo="Situação do aluno"
+         aluno="Diego"
+         nota={4}/>
+        <ComParametro 
+         titulo="Situação do aluno"
+         aluno="Barbara"
+         nota={10}/>
     </div>, 
     document.getElementById('root') 
 )
