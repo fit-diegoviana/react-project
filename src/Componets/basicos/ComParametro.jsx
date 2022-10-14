45c068a5678849b02e568ce998ad391f30f4247c
Diego <dtviana90@gmail.com>
2022-10-14 19:54:21 -0300

    chore: cria arredondamento na nota

--- src/Componets/basicos/ComParametro.jsx
+++ src/Componets/basicos/ComParametro.jsx
@@ -1,13 +1,14 @@
 export default function ComParametro(props) {
     const status = props.nota >= 7 ? 'Aprovado'
         : 'de Recuperação'
+    const notaInt = Math.ceil(props.nota)
     return (
         <div>
             <h2>{props.titulo}</h2>
             <p>
                 <strong> {props.aluno} </strong>
                 tem nota
-                <strong> {props.nota} </strong>
+                <strong> {notaInt} </strong>
                 e está
                 <strong> {status}</strong>
             </p>
