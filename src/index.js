f1d486aef91aab34b74d99f35a98ab9c509a2271
Diego <dtviana90@gmail.com>
2022-10-13 21:16:10 -0300

    feat: criação do primeiro componente e sua chamada

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import ReactDOM from "react-dom"
 import React from 'react'
 import './index.css'
+import Primeiro from "./Components/basicos/PrimeiroComponente"
 
 const tag = <strong> Oláa React!!</strong>
 ReactDOM.render
 (
     <div>
-       {tag}
+        {tag}
+        <Primeiro></Primeiro>
     </div>, 
     document.getElementById('root') 
 )
